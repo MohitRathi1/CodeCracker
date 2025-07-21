99583be6ee08688cf0df26903c8858985e138a3e
MohitRathi1 <esportsstrimer@gmail.com>
2025-07-21 21:39:11 +0530

    update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,7 +16,7 @@ const Home = () => {
 
       <div className="relative z-10 text-center px-4 max-w-4xl mx-auto">
 <img
-  src="https://ibb.co/PZZLdwW3"
+  src="https://i.ibb.co/KjjPJFsR/arrow.png"
   alt="Arrow"
   className="mx-auto"
   style={{ height: '150px' }} // Adjust as needed
